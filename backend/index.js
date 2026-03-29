1d9e0095c7bc7269e93b9ccf66275edbaf3a55cf
JulianToppo <juliantoppo95@gmail.com>
2026-03-29 22:09:50 +0530

    -feat: addition of a server with sockets for real time communication The idea of this application is to allow multiple users to edit the document simultaneously

---
+++ backend/index.js
@@ -0,0 +1,35 @@
+const http= require('http');
+const express= require('express');
+const { Server } = require('socket.io');
+
+
+const app=express();
+
+const server=http.createServer(app)
+
+const io= new Server(server,{
+    cors:{
+        origin:"*"
+    }
+})
+
+io.on('connection',(socket)=>{
+    console.log("User Connected");
+    
+    socket.on('send-message',(data)=>{
+        socket.broadcast.emit('received-message',data)
+    })
+})
+
+
+app.get("/",(req,res,next)=>{
+    try {
+        res.send("Hello World");
+    } catch (error) {
+        res.status(404)
+    }
+})
+
+server.listen(5000,()=>{
+    console.log("Server is running....")
+})
